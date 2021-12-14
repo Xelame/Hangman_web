595b42a49f1ac9c5f82cd5bad22baeb3e9d90252
AROLLAND1 <alexandre.rolland85@ynov.com>
2021-12-14 08:47:59 +0100

    merge test branch and alex

---
+++ backend.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"fmt"
+	"hangman/hangman"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type DATA struct {
+	Hangman     string
+	Word        string
+	EntryPart   string
+	Attemps     int
+	Img         string
+	LifePercent string
+}
+
+var p = DATA{
+	Hangman:     "",
+	Word:        "",
+	EntryPart:   "",
+	Attemps:     hangman.ATTEMPTS_NUMBER,
+	Img:         "",
+	LifePercent: "<div class=\"bar\"><div class=\"percentage has-tip\"  style=\"width: 100%%\" data-perc=\"100%%\"></div></div>",
+}
+
+var ancienmot string = ""
+var Tmpl404 = template.Must(template.ParseFiles("404.html"))
+var TmplHome = template.Must(template.ParseFiles("home.html"))
+var TmplTest = template.Must(template.ParseFiles("test.html"))
+
+func main() {
+
+	// Add the path where our server search assets like css/fonts/js/img
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	//     nom comprit par le serv        nom qui est dans mon pc
+
+	p.Word = "<p>" + hangman.Init(p.Attemps) + "</p>"
+
+	// Applique a chaque page une fonction qui est a l'écoute qui ecrit (ex : templates html)
+	http.HandleFunc("/home", homeHandler)
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/", errorHandler)
+
+	// Ouvre le serveur
+	fmt.Println("Open server at http://localhost:8080/home")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// Page 404 qui s'affiche si mauvaise entrer d'URL
+func errorHandler(w http.ResponseWriter, r *http.Request) {
+	Tmpl404.Execute(w, nil)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	p.Word = hangman.Init(p.Attemps)
+	p.Attemps = hangman.ATTEMPTS_NUMBER
+	p.Img = ""
+	TmplHome.Execute(w, nil)
+}
+
+// Soluce 1
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	if r.FormValue("test") != "" {
+		hangman.GuessingLetter(r.FormValue("test"))
+	}
+
+	p.Word = hangman.HideWord(hangman.WordChoosen, hangman.LettersAlreadyAppeard)
+	p.EntryPart = ListOfChoice(hangman.Solution, hangman.LettersAlreadyAppeard)
+
+	if p.Word == ancienmot {
+		p.Attemps--
+		p.LifePercent = fmt.Sprintf("<div class=\"bar\"><div class=\"percentage has-tip\"  style=\"width: %d%%\" data-perc=\"%d%%\"></div></div>", p.Attemps*10, p.Attemps*10)
+	}
+	if !(hangman.IsFinished(p.Word, p.Attemps)) {
+		if p.Attemps != 0 {
+			p.Img = "<img src=\"/assets/ff4c887a6d5eb8f92d019102cc6aba75.jpeg\"></img>"
+			p.EntryPart = ""
+		} else {
+			p.Img = "<img src=\"/assets/lose.png\"></img>"
+			p.EntryPart = ""
+			p.Word = hangman.WordChoosen
+		}
+	}
+	ancienmot = p.Word
+	TmplTest.Execute(w, p)
+}
+
+// Soluce 2 tableau de bouton
+func ListOfChoice(solution, lettersAlreadyAppeard []rune) string {
+	buttons := "<form class=\"clavier\" action=\"/test\" method=\"post\">\n\t"
+	isAlreadyAppeard := false
+	for _, letter := range solution[1:27] {
+		for _, say := range lettersAlreadyAppeard {
+			if say == letter {
+				isAlreadyAppeard = true
+			}
+		}
+		if isAlreadyAppeard {
+			buttons += fmt.Sprintf("<button class=\"unavailable\" type=\"button\">%s</button>\n\t", string(letter))
+		} else {
+			buttons += fmt.Sprintf("<button class=\"available\" type=\"submit\" name=\"test\" value=\"%s\">%s</button>\n\t", string(letter), string(letter))
+		}
+		isAlreadyAppeard = false
+	}
+	buttons += "</form>"
+	return buttons
+}
+
+/*
+func ErrorGestion(w http.ResponseWriter, r *http.Request, templateName string) {
+	templates, err := template.ParseFiles(templateName)
+	if err != nil {
+		http.Error(w, "Error 500", http.StatusInternalServerError)
+	}
+	err = templates.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error 500", http.StatusInternalServerError)
+	}
+
+}
+*/
--- hangman/guessing.go
+++ hangman/guessing.go
@@ -7,11 +7,15 @@ import "fmt"
 // -----------------------------------------------------------------------------------
 
 // Function to take player's entry with some conditions
-func GuessingButton(Guess string) {
-	for _, letter := range Guess {
-		fmt.Println(string(ToUpper(letter)))
-		lettersAlreadyAppeard = append(lettersAlreadyAppeard, ToUpper(letter)) // Add the letter in our list of guessed letters
+func GuessingLetter(input string) {
+	var letterGuessed rune = ' '
+
+	// Take the letter
+	for _, value := range input {
+		fmt.Println(value, input)
+		letterGuessed = ToUpper(rune(value)) // Put this letter in capital letter
 	}
+	IsAccentedLetter(letterGuessed) // Add the letter in our list of guessed letters
 }
 
 // Function to regroup tests to know if the player's entry is valid
@@ -26,7 +30,7 @@ func IsValidEntry(guessingInput string) bool {
 	if count == 1 {
 		guessingLetter = ToUpper(rune(guessingInput[0])) // Put this letter in capital letter
 		// Test if the letter isn't already in our list of guessed letter
-		for _, letterAlreadyHere := range lettersAlreadyAppeard {
+		for _, letterAlreadyHere := range LettersAlreadyAppeard {
 			if guessingLetter == letterAlreadyHere {
 				isNotValid = true
 			}
@@ -64,4 +68,36 @@ func ToUpper(value rune) rune {
 		value -= 32 // In ascii the capitalize letter is 32 character before the same letter but in lowercase
 	}
 	return value
+}
+
+func IsAccentedLetter(letter rune) {
+	isNotAdd := true
+	// test pour les lettres A
+	for _, test := range []rune{'A', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å'} {
+		if letter == test {
+			LettersAlreadyAppeard = append(LettersAlreadyAppeard, 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'A')
+			isNotAdd = false
+		}
+	}
+	for _, test := range []rune{'E', 'È', 'É', 'Ê', 'Ë'} {
+		if letter == test {
+			LettersAlreadyAppeard = append(LettersAlreadyAppeard, 'E', 'È', 'É', 'Ê', 'Ë')
+			isNotAdd = false
+		}
+	}
+	for _, test := range []rune{'I', 'Ì', 'Í', 'Î', 'Ï'} {
+		if letter == test {
+			LettersAlreadyAppeard = append(LettersAlreadyAppeard, 'I', 'Ì', 'Í', 'Î', 'Ï')
+			isNotAdd = false
+		}
+	}
+	for _, test := range []rune{'C', 'Ç'} {
+		if letter == test {
+			LettersAlreadyAppeard = append(LettersAlreadyAppeard, 'C', 'Ç')
+			isNotAdd = false
+		}
+	}
+	if isNotAdd {
+		LettersAlreadyAppeard = append(LettersAlreadyAppeard, letter)
+	}
 }
